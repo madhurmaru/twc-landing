68720632a0eae45767e96c23829f37c0a0e5422b
madhurmaru <155710761+madhurmaru@users.noreply.github.com>
2024-10-28 01:44:34 +0530

    Add files via upload

---
+++ app/qr/components/FilterToggle.js
@@ -0,0 +1,28 @@
+'use client';
+
+import React, { useState } from 'react';
+import nonveg from '../images/Non-veg 2.png'
+import veg from '../images/Veg 2.png'
+
+const FilterToggle = () => {
+  const [isVeg, setIsVeg] = useState(false);
+
+  const toggleFilter = () => {
+    setIsVeg(!isVeg);
+  };
+
+  return (
+    <div className="filter-toggle">
+      <img src={nonveg} alt='non-veg'></img>
+      
+      <div className={`toggle-button ${isVeg ? 'veg' : ''}`} onClick={toggleFilter}>
+        <div className="toggle-circle"></div>
+      </div>
+      
+      <img src={veg} alt='veg'></img>
+      
+    </div>
+  );
+};
+
+export default FilterToggle;
