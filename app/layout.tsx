7fb9ce6eddffcffb0ea3296dd76c692b3d59a740
kroszborg <abhimanpanwar6@gmail.com>
2024-11-23 22:45:50 +0530

    fixed fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,10 @@
 import '../styles/globals.css'
 import type { Metadata } from 'next'
-import { Aleo, Roboto } from 'next/font/google'
+import { aleo, roboto } from './fonts'
 import '../styles/animations.css'
 import '../styles/gradients.css'
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react"
-
-const aleo = Aleo({
-  subsets: ['latin'],
-  weight: ['500'],
-  variable: '--font-aleo',
-  display: 'swap',
-})
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['300', '400', '700'],
-  variable: '--font-roboto',
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: 'The Waiter Company',
--- app/menu/[tableId]/layout.tsx
+++ app/menu/[tableId]/layout.tsx
@@ -1,24 +1,9 @@
 import '../../../styles/globals.css'
 import '../../../styles/animations.css'
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
-import { Aleo } from 'next/font/google'
+import { aleo, roboto } from '../../fonts'
 import '../../../styles/gradients.css'
 import React from 'react';
-
-const aleo = Aleo({ 
-  subsets: ['latin'], 
-  weight: ['500'], 
-  variable: '--font-aleo',
-  display: 'swap',
-})
-
-const roboto = Roboto({ 
-  subsets: ['latin'], 
-  weight: ['300', '400', '700'],
-  variable: '--font-roboto',
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: 'The Waiter Company',
